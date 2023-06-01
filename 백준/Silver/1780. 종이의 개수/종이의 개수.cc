fc9c43eef7cf48a8fd083dd199deaa272871abe2
BCHSBC <121105282+BCHSBC@users.noreply.github.com>
2023-06-01 18:19:38 +0900

    [Silver II] Title: 종이의 개수, Time: 1096 ms, Memory: 20704 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.cc
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.cc
@@ -1,48 +1,52 @@
 #include <iostream>
+#include <string>
 #include <algorithm>
 #include <vector>
+#include <map>
+#include <math.h>
+#include <stack>
+#include <queue>
+#include <vector>
 using namespace std;
-
+int L[2187][2187];
 int n;
-int w,b;
-int BorW[2188][2188];
-bool check(int x,int y,int len,int flag){
-    int cnt = 0;
-    for(int i = x;i<x+len;i++){
-        for(int j = y;j<y+len;j++){
-            if(BorW[i][j] == flag){
-                cnt++;
+int cnt[3];
+bool same(int s,int x,int y){
+    int e = L[x][y];
+    for(int i = x;i < x+s;i++){
+        for(int j = y;j < y+s;j++){
+            if(L[i][j] != e){
+                return false;
             }
         }
     }
-    if(cnt == len*len){
-        return true;
-    } 
-    return false;
+    return true;
 }
-int Fold(int x,int y,int len,int flag){
-    if(len == 0) {
-        return 0;
+void check(int s,int x,int y){
+    
+    if(same(s,x,y)){
+        cnt[L[x][y]+1] ++;
+        return ;
     }
-    if(check(x,y,len,flag)){
-        return 1;
-    }
-    int res = 0;
-    for(int i = 0;i<3;i++){
-        for(int j = 0;j<3;j++){
-            res += Fold(x+i*len/3,y+j*len/3,len/3,flag);
+    
+    int new_s = s/3;
+    if(new_s == 0) return;
+    for(int i = 0;i<=new_s*2;i += new_s){
+        for(int j = 0;j<=new_s*2;j += new_s){
+            check(new_s,x+i,y+j);
         }
     }
-    return res;
+
 }
-int main() {
+int main(){
     cin >> n;
     for(int i = 0;i<n;i++){
         for(int j = 0;j<n;j++){
-            cin >> BorW[i][j];
+            cin >> L[i][j];
         }
     }
-    for(int i = -1;i<2;i++){
-        cout << Fold(0,0,n,i) << "\n";
+    check(n,0,0);
+    for(int i = 0;i<3;i++){
+        cout << cnt[i] << "\n";
     }
 }
